b93a5f8b5c1083f45d59a493c474b4926422fe57
ArcherDing <archerding@163.com>
2016-08-05 07:55:08 +0800

    init repo

---
+++ src/main/kotlin/me/archerding/services/ServicesApplication.kt
@@ -0,0 +1,11 @@
+package me.archerding.services
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class ServicesApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ServicesApplication::class.java, *args)
+}
